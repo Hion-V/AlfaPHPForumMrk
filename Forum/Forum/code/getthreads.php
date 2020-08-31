56851c9494bc5b67ab70c967ba1d803a2fa0cf5c
Hion-V <the.hion.v@gmail.com>
2020-08-31 14:56:15 +0200

    spaced query out over multiple lines

--- Forum/Forum/code/getthreads.php
+++ Forum/Forum/code/getthreads.php
@@ -1,6 +1,8 @@
 <?php
 
-$query = $conn->prepare("SELECT threads.ID, threads.board_ID, threads.user_id, threads.title, threads.date_created, users.username FROM threads INNER JOIN users ON threads.user_id=users.id");
+$query = $conn->prepare("   SELECT threads.ID, threads.board_ID, threads.user_id, threads.title, threads.date_created, users.username 
+                            FROM threads 
+                            INNER JOIN users ON threads.user_id=users.id");
 $query->execute();
 $threadArray = [];
 
