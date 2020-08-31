a45afe5c65a429bd30525ec53d8a3c85ab5ba300
Hion-V <the.hion.v@gmail.com>
2020-08-31 14:01:01 +0200

    Cleaned up the boardoverview code

--- Forum/Forum/index/content/overview_boards.php
+++ Forum/Forum/index/content/overview_boards.php
@@ -5,7 +5,21 @@ if(!isUserLoggedIn()){
 include_once('../code/getboards.php');
 //print_r($boardArray);
 //echo $_SESSION['username'];
-
+$boardrows = "";
+foreach ($boardArray as $row){
+    $boardrows .= <<< EOD
+        <tr>
+            <td>
+                <a href="?p=showthreads&board={$row[0]}">
+                    {$row[1]}
+                </a>
+            </td>
+            <td>
+                {$row[2]}
+            </td>
+        </tr>
+    EOD;
+}
 ?>
 <h1>
     Boards:
@@ -18,21 +32,6 @@ include_once('../code/getboards.php');
         </tr>
     </thead>
     <tbody>
-<?php
-foreach ($boardArray as $row){
-?>
-        <tr>
-            <td>
-                <a href="?p=showthreads&board=<?=$row[0]?>">
-                    <?=$row[1]?>
-                </a>
-            </td>
-            <td>
-                <?=$row[2]?>
-            </td>
-        </tr>
-<?php
-    }
-?>
+        <?=$boardrows?>
     </tbody>
 </table>
